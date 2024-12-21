6f3873e1e28be592daf8b0911e674f1cd1752f11
Pranav <pranavkp.me@outlook.com>
2024-12-21 14:39:05 +0530

    details token added

---
+++ src/app/token/[address]/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import useTokenDetailsStore from "@/store/useTokenDetailsStore";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+
+const Page = () => {
+  const { address } = useParams() as { address: string };
+  const { fetchTokens, data, isLoading } = useTokenDetailsStore();
+
+  useEffect(() => {
+    fetchTokens(address);
+  }, []);
+
+  if (isLoading || !data) {
+    <div>Loading</div>;
+  }
+  console.log(data);
+  if (data) {
+    return (
+      <div>
+        <div>
+          <div key={data.mintAddress}>
+            <img src={data.imgUrl} alt="token image" />
+            <p>Create by {data.creatorWalletAddress}</p>
+            <p>{data.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return <div>No token found</div>;
+};
+
+export default Page;
---
+++ src/store/useTokenDetailsStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { Coin } from "../../type";
+import api from "@/lib/axios";
+
+interface TokenDetailsStore {
+  data: Coin | null;
+  isLoading: boolean;
+  error: string | null;
+  fetchTokens: (address: string) => Promise<void>;
+  mutate: (updater: (currentData: Coin | null) => Coin | null) => void;
+}
+
+const useTokenDetailsStore = create<TokenDetailsStore>((set) => ({
+  data: null,
+  isLoading: false,
+  error: null,
+  fetchTokens: async (address: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const { data } = await api.get<Coin>(
+        `/v1/token/details?token=${address}`
+      );
+      console.log(data);
+      set({ data: data, isLoading: false });
+    } catch (error: any) {
+      set({ error: error.message, isLoading: false });
+    }
+  },
+  mutate: (updater) => {
+    set((state) => ({ data: updater(state.data) }));
+  },
+}));
+
+export default useTokenDetailsStore;
