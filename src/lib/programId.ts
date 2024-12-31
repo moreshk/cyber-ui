c5347a46fa8619b2b413e55d6c49f2ef58294572
Viraj Kokane <virajkokane123@gmail.com>
2024-12-31 09:11:25 +0530

    new program id

--- src/lib/programId.ts
+++ src/lib/programId.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const PROGRAM_ID_IDL = new PublicKey(
-  "37FL1tUEA71braP92DKChGbXKNpx8Mg4MNv8BuEsukp8"
+  "95pm5hRqWdb2rRzvh8MDT8pvtPFXk6seBUo8yhaCjtfD"
 );
 
 export const PROGRAM_ID: PublicKey = PROGRAM_ID_IDL;
