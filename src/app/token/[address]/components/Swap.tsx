16b26e760c7778b18d5e4aa1530a9890682fc07a
uchihaa619 <uchihaa.clan619@gmail.com>
2025-01-02 09:53:33 +0530

    fix

--- src/app/token/[address]/components/Swap.tsx
+++ src/app/token/[address]/components/Swap.tsx
@@ -3,37 +3,28 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import BuyToken from "./BuyToken";
 import SellToken from "./SellToken";
-import { useEffect, useState } from "react";
-import api from "@/lib/axios";
-import useTokenDetailsStore from "@/store/useTokenDetailsStore";
 
 export default function Swap() {
-  const { data } = useTokenDetailsStore();
-  const [migrtion, setMigration] = useState(false);
+  // const migration = async () => {
+  //   try {
+  //     setMigration(true);
+  //     await api.get(`/v1/token/migration?token=${data?.mintAddress}`);
+  //     setMigration(true);
+  //   } catch (e) {
+  //     setMigration(false);
+  //     console.log(e);
+  //   }
+  // };
 
-  const migration = async () => {
-    try {
-      setMigration(true);
-      await api.get(`/v1/token/migration?token=${data?.mintAddress}`);
-      setMigration(true);
-    } catch (e) {
-      setMigration(false);
-      console.log(e);
-    }
-  };
-  useEffect(() => {
-    migration();
-  }, []);
+  // useEffect(() => {
+  //   migration();
+  // }, []);
 
   return (
     <Tabs defaultValue="buy" className="w-full">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="buy" disabled={migrtion}>
-          Buy
-        </TabsTrigger>
-        <TabsTrigger value="sell" disabled={migrtion}>
-          Sell
-        </TabsTrigger>
+        <TabsTrigger value="buy">Buy</TabsTrigger>
+        <TabsTrigger value="sell">Sell</TabsTrigger>
       </TabsList>
       <TabsContent value="buy">
         <BuyToken />
